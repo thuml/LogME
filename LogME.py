5024ef031749ccf18b8c79e03722c7bdbc684022
youkaichao <youkaichao@126.com>
2021-08-08 16:13:31 +0800

    sklearn like api

--- LogME.py
+++ LogME.py
@@ -1,4 +1,5 @@
-import torch
+import warnings
+
 import numpy as np
 from numba import njit
 
@@ -8,10 +9,11 @@ def each_evidence(y_, f, fh, v, s, vh, N, D):
     """
     compute the maximum evidence for each class
     """
+    epsilon = 1e-5
     alpha = 1.0
     beta = 1.0
     lam = alpha / beta
-    tmp = (vh @ (f @ y_))
+    tmp = (vh @ (f @ np.ascontiguousarray(y_)))
     for _ in range(11):
         # should converge after at most 10 steps
         # typically converge after two or three steps
@@ -20,9 +22,9 @@ def each_evidence(y_, f, fh, v, s, vh, N, D):
         # A_inv = v @ np.diag(1.0 / (alpha + beta * s)) @ v.transpose() # no need to compute A_inv
         m = v @ (tmp * beta / (alpha + beta * s))
         alpha_de = (m * m).sum()
-        alpha = gamma / alpha_de
+        alpha = gamma / (alpha_de + epsilon)
         beta_de = ((y_ - fh @ m) ** 2).sum()
-        beta = (N - gamma) / beta_de
+        beta = (N - gamma) / (beta_de + epsilon)
         new_lam = alpha / beta
         if np.abs(new_lam - lam) / lam < 0.01:
             break
@@ -30,10 +32,10 @@ def each_evidence(y_, f, fh, v, s, vh, N, D):
     evidence = D / 2.0 * np.log(alpha) \
                + N / 2.0 * np.log(beta) \
                - 0.5 * np.sum(np.log(alpha + beta * s)) \
-               - beta / 2.0 * beta_de \
-               - alpha / 2.0 * alpha_de \
+               - beta / 2.0 * (beta_de + epsilon) \
+               - alpha / 2.0 * (alpha_de + epsilon) \
                - N / 2.0 * np.log(2 * np.pi)
-    return evidence / N
+    return evidence / N, alpha, beta, m
 
 
 # use pseudo data to compile the function
@@ -42,36 +44,96 @@ f_tmp = np.random.randn(20, 50).astype(np.float64)
 each_evidence(np.random.randint(0, 2, 50).astype(np.float64), f_tmp, f_tmp.transpose(), np.eye(20, dtype=np.float64), np.ones(20, dtype=np.float64), np.eye(20, dtype=np.float64), 50, 20)
 
 
-def LogME(f: torch.Tensor, y: torch.Tensor, regression=False):
-    """
-    :param f: [N, F], feature matrix from pre-trained model
-    :param y: target labels.
-        For classification, y has shape [N] with element in [0, C_t).
-        For regression, y has shape [N, C] with C regression-labels
-    :param regression: whether regression
-    :return: LogME score (how well f can fit y directly)
-    """
-    f = f.detach().cpu().numpy().astype(np.float64)
-    y = y.detach().cpu().numpy()
-    if regression:
-        y = y.astype(np.float64)
+class LogME(object):
+    def __init__(self, regression=False):
+        """
+            :param regression: whether regression
+        """
+        self.regression = regression
+        self.fitted = False
+        self.reset()
 
-    fh = f
-    f = f.transpose()
-    D, N = f.shape
-    v, s, vh = np.linalg.svd(f @ fh, full_matrices=True)
+    def reset(self):
+        self.num_dim = 0
+        self.alphas = []
+        self.betas = []
+        self.ms = []
 
-    evidences = []
-    if regression:
-        K = y.shape[1]
-        for i in range(K):
-            y_ = y[:, i]
-            evidence = each_evidence(y_, f, fh, v, s, vh, N, D)
+    def fit(self, f: np.ndarray, y: np.ndarray):
+        """
+        :param f: [N, F], feature matrix from pre-trained model
+        :param y: target labels.
+            For classification, y has shape [N] with element in [0, C_t).
+            For regression, y has shape [N, C] with C regression-labels
+
+        :return: LogME score (how well f can fit y directly)
+        """
+        if self.fitted:
+            warnings.warn('re-fitting for new data. old parameters cleared.')
+            self.reset()
+        else:
+            self.fitted = True
+        f = f.astype(np.float64)
+        if self.regression:
+            y = y.astype(np.float64)
+
+        fh = f
+        f = f.transpose()
+        D, N = f.shape
+        v, s, vh = np.linalg.svd(f @ fh, full_matrices=True)
+
+        evidences = []
+        self.num_dim = y.shape[1] if self.regression else int(y.max() + 1)
+        for i in range(self.num_dim):
+            y_ = y[:, i] if self.regression else (y == i).astype(np.float64)
+            evidence, alpha, beta, m = each_evidence(y_, f, fh, v, s, vh, N, D)
             evidences.append(evidence)
-    else:
-        K = int(y.max() + 1)
-        for i in range(K):
-            y_ = (y == i).astype(np.float64)
-            evidence = each_evidence(y_, f, fh, v, s, vh, N, D)
-            evidences.append(evidence)
-    return np.mean(evidences)
+            self.alphas.append(alpha)
+            self.betas.append(beta)
+            self.ms.append(m)
+        self.ms = np.stack(self.ms)
+        return np.mean(evidences)
+
+    def predict(self, f: np.ndarray):
+        """
+        :param f: [N, F], feature matrix
+        :return: prediction, return shape [N, X]
+        """
+        if not self.fitted:
+            raise RuntimeError("not fitted, please call fit first")
+        f = f.astype(np.float64)
+        logits = f @ self.ms.T
+        if self.regression:
+            return logits
+        return np.argmax(logits, axis=-1)
+
+    def probability(self, f: np.ndarray, y: np.ndarray):
+        """
+        :param f: [N, F], feature matrix
+        :param y: target labels.
+            For classification, y has shape [N] with element in [0, C_t).
+            For regression, y has shape [N, C] with C regression-labels
+
+        :return: probability score (how well each f can fit each y directly), return shape [N]
+        """
+        if not self.fitted:
+            raise RuntimeError("not fitted, please call fit first")
+        f = f.astype(np.float64)
+        if self.regression:
+            y = y.astype(np.float64)
+        scores = []
+        D = f.shape[1]
+        for i in range(self.num_dim):
+            alpha = self.alphas[i]
+            beta = self.betas[i]
+            y_ = y[:, i] if self.regression else (y == i).astype(np.float64)
+            common_term = 0.5 * np.log(beta) + D / 2 * np.log(alpha) - 0.5 * np.log(2 * np.pi)
+            beta_uTu = beta * np.sum((f * f), axis=1, keepdims=True)
+            ms_de = alpha + beta_uTu
+            ms = beta * y_.reshape(-1, 1) * f / ms_de
+            logdetAs = (D - 1) * np.log(alpha) + np.log(ms_de.reshape(-1))
+            err = (np.sum(ms * f, axis=1) - y_) ** 2
+            ms_norm = np.sum(ms * ms, axis=1)
+            ans = common_term - 0.5 * beta * err - 0.5 * alpha * ms_norm - 0.5 * logdetAs
+            scores.append(ans)
+        return np.mean(np.stack(scores), axis=0)
