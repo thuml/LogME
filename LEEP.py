f56eab08445285495762b84ccf92c96a1cc8744b
youkaichao <youkaichao@126.com>
2021-10-13 09:27:56 +0800

    add LEEP.py

---
+++ LEEP.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def LEEP(pseudo_source_label: np.ndarray, target_label: np.ndarray):
+    """
+
+    :param pseudo_source_label: shape [N, C_s]
+    :param target_label: shape [N], elements in [0, C_t)
+    :return: leep score
+    """
+    N, C_s = pseudo_source_label.shape
+    target_label = target_label.reshape(-1)
+    C_t = int(np.max(target_label) + 1)   # the number of target classes
+    normalized_prob = pseudo_source_label / float(N)  # sum(normalized_prob) = 1
+    joint = np.zeros((C_t, C_s), dtype=float)  # placeholder for joint distribution over (y, z)
+    for i in range(C_t):
+        this_class = normalized_prob[target_label == i]
+        row = np.sum(this_class, axis=0)
+        joint[i] = row
+    p_target_given_source = (joint / joint.sum(axis=1, keepdims=True)).T  # P(y | z)
+
+    empirical_prediction = pseudo_source_label @ p_target_given_source
+    empirical_prob = np.array([predict[label] for predict, label in zip(empirical_prediction, target_label)])
+    leep_score = np.mean(np.log(empirical_prob))
+    return leep_score
